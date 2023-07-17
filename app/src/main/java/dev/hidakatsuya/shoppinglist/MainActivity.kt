4af79fe054059918793d42977dda02f1a4551abc
Katsuya Hidaka <hidakatsuya@gmail.com>
2023-07-18 03:30:45 +0900

    implement app bar and floating button
    However, actions are implemented as dummies.

--- app/src/main/java/dev/hidakatsuya/shoppinglist/MainActivity.kt
+++ app/src/main/java/dev/hidakatsuya/shoppinglist/MainActivity.kt
@@ -4,9 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import dev.hidakatsuya.shoppinglist.feature.ShoppingListScreen
 import dev.hidakatsuya.shoppinglist.ui.theme.ShoppingListTheme
 
@@ -14,14 +29,52 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ShoppingListTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+            RootScreen()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RootScreen() {
+    ShoppingListTheme {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            containerColor = MaterialTheme.colorScheme.background,
+            topBar = {
+                CenterAlignedTopAppBar(
+                    title = {
+                        Text(stringResource(id = R.string.app_name))
+                    },
+                    actions = {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                imageVector = Icons.Filled.MoreVert,
+                                contentDescription = "More"
+                            )
+                        }
+                    }
+                )
+            },
+            floatingActionButtonPosition = FabPosition.End,
+            floatingActionButton = {
+                FloatingActionButton(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Filled.Add, "Delete")
+                }
+            },
+            content = { innerPadding ->
+                Surface(modifier = Modifier.padding(innerPadding)) {
                     ShoppingListScreen()
                 }
             }
-        }
+        )
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+private fun RootScreenPreview() {
+    ShoppingListTheme {
+        RootScreen()
     }
 }
