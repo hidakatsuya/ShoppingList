be92d1d668b091c33e1116da4ec918d25c00641e
Katsuya Hidaka <hidakatsuya@gmail.com>
2023-07-19 01:23:58 +0900

    Move ui implementation for ItemsListScreen to ShoppingListScreen

--- app/src/main/java/dev/hidakatsuya/shoppinglist/MainActivity.kt
+++ app/src/main/java/dev/hidakatsuya/shoppinglist/MainActivity.kt
@@ -3,24 +3,7 @@ package dev.hidakatsuya.shoppinglist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FabPosition
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import dev.hidakatsuya.shoppinglist.feature.ShoppingListScreen
 import dev.hidakatsuya.shoppinglist.ui.theme.ShoppingListTheme
@@ -29,45 +12,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RootScreen()
+            ShoppingListScreen()
         }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun RootScreen() {
-    ShoppingListTheme {
-        Scaffold(
-            modifier = Modifier.fillMaxSize(),
-            containerColor = MaterialTheme.colorScheme.background,
-            topBar = {
-                CenterAlignedTopAppBar(
-                    title = {
-                        Text(stringResource(id = R.string.app_name))
-                    },
-                    actions = {
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(
-                                imageVector = Icons.Filled.MoreVert,
-                                contentDescription = "More"
-                            )
-                        }
-                    }
-                )
-            },
-            floatingActionButtonPosition = FabPosition.End,
-            floatingActionButton = {
-                FloatingActionButton(onClick = { /*TODO*/ }) {
-                    Icon(Icons.Filled.Add, "Delete")
-                }
-            },
-            content = { innerPadding ->
-                Surface(modifier = Modifier.padding(innerPadding)) {
-                    ShoppingListScreen()
-                }
-            }
-        )
     }
 }
 
@@ -75,6 +21,6 @@ fun RootScreen() {
 @Composable
 private fun RootScreenPreview() {
     ShoppingListTheme {
-        RootScreen()
+        ShoppingListScreen()
     }
 }
